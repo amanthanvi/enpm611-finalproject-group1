4a1281a7fe6039bf0466e34cf69b3c05b1d04e0f
ethankrm <kramer17@umd.edu>
2025-03-28 09:44:15 -0400

    worked more on duplicates function so that it now charts the users who create the most duplicate issues, and who reports the most. Still needs clean up

--- duplicates.py
+++ duplicates.py
@@ -26,6 +26,7 @@ class Duplicates:
         
         duplicates:int = 0  
         duplicate_owner = []
+        duplicate_reporter = []
         for issue in issues:        
             check = False
             for e in issue.events:
@@ -35,9 +36,25 @@ class Duplicates:
                         check = True
                         print(issue.number)
                         duplicate_owner.append(issue.creator)
+                        duplicate_reporter.append(e.author)
                 
         print('\n\n'+str(duplicates)+'\n\n')
         
+                ### BAR CHART
+        top_n:int = 50
+        title1:string = f"Top {top_n} duplicate issue creators"
+        df = pd.DataFrame(duplicate_owner)
+        df_hist = df.value_counts().nlargest(top_n).plot(kind="bar", figsize=(14,8), title=title1)
+        df_hist.set_xlabel("Creator Names")
+        df_hist.set_ylabel("# of issues created that were duplicates")
+        plt.show() 
+
+        title2:string = f"Top {top_n} duplicate issue reporters"
+        df = pd.DataFrame(duplicate_reporter)
+        df_hist = df.value_counts().nlargest(top_n).plot(kind="bar", figsize=(14,8), title = title2)
+        df_hist.set_xlabel("Creator Names")
+        df_hist.set_ylabel("# of issues that author reported as duplicate")
+        plt.show() 
     
 
 if __name__ == '__main__':
